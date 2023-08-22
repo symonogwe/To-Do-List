7753fe36cb80bff7721d13254ba538b79cadaaa1
symonogwe <symonogwe@gmail.com>
2023-08-22 15:47:02 +0300

    Create styles folder and add index.css
    - Add nav styles to nav elements
    - Give logo icon src using javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
+import "./styles/index.css";
+import logo from "./assets/logo.svg"
 import { createProject, projectsArray } from "./project.js";
 import { taskDone } from "./task.js";
 
 
-
-
-
-
-
+// NAV SECTION
+const logoIcon = document.querySelector(".logo-icon");
+logoIcon.src = logo;

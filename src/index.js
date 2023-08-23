4c832f63251339dcdb53c9f788c4e1cadbbd78b5
symonogwe <symonogwe@gmail.com>
2023-08-23 10:57:36 +0300

    Finish creating left-side-bar elements
    - Add necessary styles to the items
    - Add icons to assets and set their src using javascript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 import "./styles/index.css";
-import logo from "./assets/logo.svg"
+
+import logo from "./assets/logo.svg";
+import homeDivIcon from "./assets/home.svg";
+import dateDivIcon from "./assets/day.svg";
+import addIcon from "./assets/add.svg";
+
 import { createProject, projectsArray } from "./project.js";
 import { taskDone } from "./task.js";
 
@@ -8,3 +13,16 @@ import { taskDone } from "./task.js";
 // NAV SECTION
 const logoIcon = document.querySelector(".logo-icon");
 logoIcon.src = logo;
+
+// LEFT SIDE-BAR (HOME SECTION)
+const homeImg = document.getElementById("home-div-icon");
+homeImg.src = homeDivIcon;
+
+const todayImg = document.getElementById("today-div-icon");
+todayImg.src = dateDivIcon;
+
+const thisWeekImg = document.getElementById("week-div-icon");
+thisWeekImg.src = dateDivIcon;
+
+const projectPlusImg = document.querySelector(".project-plus-icon");
+projectPlusImg.src = addIcon;

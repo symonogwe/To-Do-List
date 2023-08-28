8fc9d48cac19770243e9bf5f6906a5950651c477
symonogwe <symonogwe@gmail.com>
2023-08-28 13:48:06 +0300

    Delete unused function in task module

--- src/task.js
+++ src/task.js
@@ -23,13 +23,6 @@ function createTask() {
     hideTaskForm();
 }
 
-// function that checks whether task is done
-function taskDone(project) {
-    project.taskArray.forEach(task => {
-        if (task.checkList === true) {
-            console.log(task.title + ` task in ${project.name} project has been completed`);
-        }
-    })
-}
 
-export { Task, taskDone, createTask};
+
+export { Task, createTask};

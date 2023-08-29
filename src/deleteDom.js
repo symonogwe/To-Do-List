d267f843d31fe22736e164fdb41fe1f434436105
symonogwe <symonogwe@gmail.com>
2023-08-29 13:29:12 +0300

    Modify appearance

--- src/deleteDom.js
+++ src/deleteDom.js
@@ -14,17 +14,11 @@ function deleteProject(e) {
 // TASKS COMPLETE & DELETE
 let completedDiv = [];
 function getFinishTask(e) {
-    // console.log(e.target.parentElement.parentElement);
     let completedDivIndex = e.target.parentElement.parentElement.dataset.index;
     completedDiv.push(targetObject.taskArray[completedDivIndex]);
     createProjectTasks();
     console.log(completedDiv);
 }
-
-// function finishTask() {
-//     console.log(targetObject.taskArray[completedDivIndex]);
-// }
-
 
 function deleteTask(e) {
     let targetIndex = e.target.parentElement.parentElement.dataset.index;
